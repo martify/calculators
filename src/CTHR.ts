8cbacb08a1ad898e2548a49c2f3ecbbce5b14173
Victor Sandahl <victor.sandahl99@gmail.com>
2022-09-14 15:42:19 +0200

    Functional functions and tests

---
+++ src/CTHR.ts
@@ -0,0 +1,36 @@
+type Criteria = {
+    GCS_15: boolean,
+    hasFracture: boolean,
+    hasBaseFracture: boolean,
+    hasThrownUp: boolean,
+    isOver65: boolean,
+    hasAmnesia:boolean,
+    dangerousMech: boolean
+}
+
+function calculatePoints(fields: Criteria): number {
+    let points = 0
+    Object.values(fields).forEach(value => {
+      if (value) {
+        points++
+      }
+    })
+    return points
+  }
+
+  function pointsToText(points: number) {
+    if (points >= 1){
+        return 'CT skalle rekommenderas'
+    }
+    else {
+        return 'CT skalle behövs inte'
+    }
+  }
+
+  function calcHeadRule(fields: Criteria){
+    const points = calculatePoints(fields)
+    const message = pointsToText(points)
+    return message
+  }
+
+export {calcHeadRule}
---
+++ src/__tests__/CTHR.ts
@@ -0,0 +1,57 @@
+import {calcHeadRule} from '../CTHR'
+
+test('7 points', () => {
+    const fields = {
+        GCS_15: true,
+        hasFracture: true,
+        hasBaseFracture: true,
+        hasThrownUp: true,
+        isOver65: true,
+        hasAmnesia: true,
+        dangerousMech: true
+    }
+    const message = calcHeadRule(fields)
+    expect(message).toEqual('CT skalle rekommenderas')
+})
+
+test('4 points', () => {
+    const fields = {
+        GCS_15: false,
+        hasFracture: true,
+        hasBaseFracture: true,
+        hasThrownUp: true,
+        isOver65: false,
+        hasAmnesia: true,
+        dangerousMech: false
+    }
+    const message = calcHeadRule(fields)
+    expect(message).toEqual('CT skalle rekommenderas')
+})
+
+test('1 points', () => {
+    const fields = {
+        GCS_15: true,
+        hasFracture: false,
+        hasBaseFracture: false,
+        hasThrownUp: false,
+        isOver65: false,
+        hasAmnesia: false,
+        dangerousMech: false
+    }
+    const message = calcHeadRule(fields)
+    expect(message).toEqual('CT skalle rekommenderas')
+})
+
+test('0 points', () => {
+    const fields = {
+        GCS_15: false,
+        hasFracture: false,
+        hasBaseFracture: false,
+        hasThrownUp: false,
+        isOver65: false,
+        hasAmnesia: false,
+        dangerousMech: false
+    }
+    const message = calcHeadRule(fields)
+    expect(message).toEqual('CT skalle behövs inte')
+})
