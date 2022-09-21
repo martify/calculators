3a5fa37f3391277a099a86e5968b62efe051588c
Victor Sandahl <victor.sandahl99@gmail.com>
2022-09-21 14:50:22 +0200

    Added more coverage of tests

--- src/__tests__/nihss.ts
+++ src/__tests__/nihss.ts
@@ -4,21 +4,12 @@ test('it should return 17', () => {
     const fields = {
         correctMonth: true,
         correctAge: true,
-        correctEyes: true,
-        correctHand: true,
-    }
-    expect(calculateStrokeScale(1, fields, 0, 2, 0, 3, 4, 0, 0, 0, 2, 5, 0, 0)).toEqual(17)
-})
-
-test('it should return 30', () => {
-    const fields = {
-        correctMonth: true,
-        correctAge: true,
         correctEyes: false,
         correctHand: false,
     }
-    expect(calculateStrokeScale(2, fields, 1, 2, 1, 3, 4, 0, 0, 4, 2, 3, 2, 4)).toEqual(30)
+    expect(calculateStrokeScale(1, fields, 0, 2, 0, 3, 4, 0, 0, 0, 2, 5, 0, 0)).toEqual(19)
 })
+
 
 test('it should return 0', () => {
     const fields = {
@@ -37,5 +28,5 @@ test('it should return 0', () => {
         correctEyes: true,
         correctHand: true,
     }
-    expect(calculateStrokeScale(3, fields, 2, 3, 3, 5, 5, 5, 5, 2, 2, 3, 3, 2)).toEqual(43)
+    expect(calculateStrokeScale(3, fields, 2, 3, 3, 4, 4, 4, 4, 2, 2, 3, 3, 2)).toEqual(39)
 })
