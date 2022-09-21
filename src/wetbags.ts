915b0f55df47eed3a9d11f25a204e777b02c9032
Victor Sandahl <victor.sandahl99@gmail.com>
2022-09-21 09:09:47 +0200

    Changed age calculation and declaration of local variables

--- src/wetbags.ts
+++ src/wetbags.ts
@@ -6,9 +6,10 @@ enum AgeDefine {
 const isValidAge = (age: unknown): boolean =>
     typeof age === 'number' && age >= 0 && age <= 12 
 
-function getWeight(age: number, definition: AgeDefine) {
-    if (age < 1 || definition === AgeDefine.Months) {
-        return age / 2 + 4
+
+function getWeight(age: number) {
+    if (age < 1) {
+        return age * 6 + 4
     }
     else if (age >= 1 && age <=5) {
         return age * 2 + 8
@@ -18,17 +19,12 @@ function getWeight(age: number, definition: AgeDefine) {
     }
 }
 
-function getTube(age: number, definition: AgeDefine) {
-    if (definition === AgeDefine.Months){
-        return Math.round((age / 48 + 4) * 1000) / 1000
-    }
-    else {
-        return Math.round((age / 4 + 4) * 1000) / 1000
-    }
+function getTube(age: number) {
+    return Math.round((age / 4 + 4) * 1000) / 1000
 }
 
-function getAlbuterol(age: number, definition: AgeDefine){
-    if (age < 5 || definition === AgeDefine.Months){
+function getAlbuterol(age: number){
+    if (age < 5){
         return 2.5
     }
     else {
@@ -40,23 +36,27 @@ function getWetbags(age: number, definition: AgeDefine) {
     if (!isValidAge(age)){
         throw new Error('Invalid age for wetbags')
     }
-    const weight = getWeight(age, definition)
-    const defib = weight * 4
-    const conversion = weight
-    const tube = getTube(age, definition)
-    const bolus = 20 * weight
-    const bolus_trauma = 10 * weight
-    const adrenalin = 0.01 * weight
-    const glucose = 2 * weight
-    const stesolid_iv = 0.25 * weight
-    const stesolid_pr = 0.5 * weight
-    const adrenalin_inh = 5
-    const albuterol = getAlbuterol(age, definition)
-    const atrovent = 0.25
-    const atropin = 0.02 * weight
-    const amiodaron = 5 * weight
-    return {weight, defib, conversion, tube, bolus, bolus_trauma, adrenalin, glucose,
-            stesolid_iv, stesolid_pr, adrenalin_inh, albuterol, atrovent, atropin, amiodaron}
+
+    const ageInYears = definition === AgeDefine.Months ? age / 12 : age
+    const weight = getWeight(ageInYears)
+
+    return {
+        weight, 
+        defib: weight * 4, 
+        conversion: weight, 
+        tube: getTube(ageInYears), 
+        bolus: 20 * weight, 
+        bolus_trauma: 10 * weight, 
+        adrenalin: 0.01 * weight, 
+        glucose: 2 * weight,
+        stesolid_iv: 0.25 * weight, 
+        stesolid_pr: 0.5 * weight, 
+        adrenalin_inh: 5, 
+        albuterol: getAlbuterol(ageInYears), 
+        atrovent: 0.25, 
+        atropin: 0.02 * weight, 
+        amiodaron: 5 * weight
+    }
 }
 
 export {getWetbags, isValidAge}
