92fcd187388050325c6dc2457ebc6c7a6b235cc6
Victor Sandahl <victor.sandahl99@gmail.com>
2022-09-22 08:22:20 +0200

    Functional tests

---
+++ src/__tests__/ottawaAnkle.ts
@@ -0,0 +1,40 @@
+import { getAncleRule } from "../ottawaAnkle"
+
+
+test("score 3", () => {
+    const fields = {
+        immediateProblem: true,
+        delayedProblem: true,
+        isSoreOB: true,
+    }
+    expect(getAncleRule(fields)).toEqual("Röntgen av mellanfoten rekommenderas")
+})
+
+test("score 4", () => {
+    const fields = {
+        immediateProblem: true,
+        delayedProblem: true,
+        isSoreOB: true,
+        isSoreON: true
+    }
+    expect(getAncleRule(fields)).toEqual("Röntgen av mellanfoten rekommenderas")
+})
+
+test("score 0", () => {
+    const fields = {
+        immediateProblem: false,
+        delayedProblem: false,
+        isSoreOB: false,
+    }
+    expect(getAncleRule(fields)).toEqual("Röntgen av mellanfoten ej nödvändig")
+})
+
+test("score 0", () => {
+    const fields = {
+        immediateProblem: false,
+        delayedProblem: false,
+        isSoreOB: false,
+        isSoreON: false
+    }
+    expect(getAncleRule(fields)).toEqual("Röntgen av mellanfoten ej nödvändig")
+})
