888a12219fff3d0c405f98b869d5650576451a22
Victor Sandahl <victor.sandahl99@gmail.com>
2022-09-22 15:51:40 +0200

    Functional functions and tests

---
+++ src/__tests__/ogonsjukdomar.ts
@@ -0,0 +1,43 @@
+import { getDiagnoses } from "../ogonsjukdomar"
+
+test('it should show ordered risks correctly', () => {
+    const fields = {
+    redEyes: false,
+    chafeFeel: false,
+    lightSens: true,
+    hasItch: false,
+    hasPus: false,
+    hasPain: false,
+    movingSpots: true,
+    hasFlicker: false,
+    darkShadow: true,
+    hookSight: false,
+    coloredRings: false
+    }
+    expect(getDiagnoses(fields)).toEqual({
+        "1": ["hinna", "irit"], 
+        "2": ["glaskropp", "keratit"], 
+        "3": ["makula", "konjunk", "glaukom"]
+    })
+})
+
+test('it should show ordered risks correctly', () => {
+    const fields = {
+    redEyes: true,
+    chafeFeel: true,
+    lightSens: false,
+    hasItch: true,
+    hasPus: true,
+    hasPain: true,
+    movingSpots: false,
+    hasFlicker: true,
+    darkShadow: false,
+    hookSight: true,
+    coloredRings: true
+    }
+    expect(getDiagnoses(fields)).toEqual({
+        "1": ["konjunk", "glaukom"], 
+        "2": ["keratit", "irit"], 
+        "3": ["makula", "hinna", "glaskropp"]
+    })
+})
---
+++ src/ogonsjukdomar.ts
@@ -0,0 +1,101 @@
+type Symptoms = {
+    redEyes: boolean,
+    chafeFeel: boolean,
+    lightSens: boolean,
+    hasItch: boolean,
+    hasPus: boolean,
+    hasPain: boolean,
+    movingSpots: boolean,
+    hasFlicker: boolean,
+    darkShadow: boolean,
+    hookSight: boolean,
+    coloredRings: boolean
+}
+
+function calculateSymptomes(fields: Symptoms) {
+    let probScale = {irit: 0, keratit: 0, glaukom: 0, konjunk: 0, glaskropp: 0, hinna: 0, makula: 0}
+    if (fields.redEyes) {
+        probScale.irit++
+        probScale.keratit++
+        probScale.konjunk++
+        probScale.glaukom++
+    }
+    if (fields.chafeFeel) {
+        probScale.keratit++
+    }
+    if (fields.lightSens) {
+        probScale.irit++
+        probScale.keratit++
+    }
+    if (fields.hasItch) {
+        probScale.konjunk++
+    }
+    if (fields.hasPus) {
+        probScale.konjunk++
+    }
+    if (fields.hasPain) {
+        probScale.irit++
+        probScale.glaukom++
+    }
+    if (fields.movingSpots) {
+        probScale.irit++
+        probScale.glaskropp++
+        probScale.hinna++
+    }
+    if (fields.hasFlicker) {
+        probScale.glaskropp++
+        probScale.hinna++
+    }
+    if (fields.darkShadow) {
+        probScale.hinna++
+    }
+    if (fields.hookSight) {
+        probScale.makula++
+    }
+    if (fields.coloredRings) {
+        probScale.glaukom++
+    }
+    return probScale
+}
+
+function sortDiagnoses(probScale: object) {
+   return Object.entries(probScale).sort((a, b) => {
+                if (b[1] > a[1]) {
+                    return 1
+                }
+                else {
+                    return -1
+                }
+            })
+}
+
+function mergeDiagnoses(sortedScale: [string, any][]) {
+    let final = {}
+    let place = 0
+    let lastValue = 0
+    sortedScale.forEach(value => {
+        let diagnoses = value[0]
+        let rank = value[1]
+        console.log(final)
+        if(lastValue != rank) {
+            place++
+            final = {...final, [place]: [diagnoses]}
+            lastValue = rank
+        }
+        else {
+            console.log(place)
+            final[place].push(diagnoses)
+        }
+
+    })
+    console.log(final)
+    return final
+} 
+
+function getDiagnoses(fields: Symptoms) {
+    const probScale = calculateSymptomes(fields)
+    const sortedScale =  sortDiagnoses(probScale)
+    return mergeDiagnoses(sortedScale)
+}
+
+export {getDiagnoses}
