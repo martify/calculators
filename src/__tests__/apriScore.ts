86425a866851cf757b83722d5dd24914ecd27892
Victor Sandahl <victor.sandahl99@gmail.com>
2022-10-05 16:07:29 +0200

    Final functions and tests done

--- src/__tests__/apriScore.ts
+++ src/__tests__/apriScore.ts
@@ -1,10 +1,10 @@
 import {getAPRIScore} from "../apriScore"
 
 test.each`
-    ast | plateletCount | expected
-    ${49}   ${332}        ${0.37}
-    ${78}   ${155}        ${1.26}
-    ${25}   ${210}        ${0.3}
-`('It should return the expected value', ({ast, plateletCount, expected}) => {
-    expect(getAPRIScore(ast, plateletCount)).toEqual(expected)
+    asat |  tpk  |  male  | expected
+    ${49}  ${332}   ${true}  ${19.7}
+    ${78}  ${155}   ${false} ${83.9}
+    ${25}  ${210}   ${true}  ${15.9}
+`('It should return the expected value', ({asat, tpk, male, expected}) => {
+    expect(getAPRIScore(asat, tpk, male)).toEqual(expected)
 })
--- src/apriScore.ts
+++ src/apriScore.ts
@@ -1,7 +1,10 @@
 
 
-function getAPRIScore(ast: number, plateletCount: number) {
-    return Math.round((ast / 40) / plateletCount * 10000) / 100
+function getAPRIScore(asat: number, tpk: number, male: boolean) {
+    if (male) {
+        return Math.round((asat / (0.6 * 1.25) / tpk) * 1000) /10
+    }
+    return Math.round((asat / (0.6 * 1) / tpk) * 1000) / 10
 }
 
 export {getAPRIScore}
