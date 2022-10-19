d9a7b0fb8c1dc01c798f419f82cd4107424b1295
Victor Sandahl <victor.sandahl99@gmail.com>
2022-10-19 10:47:17 +0200

    Ready for PR

--- src/__tests__/mdrd.ts
+++ src/__tests__/mdrd.ts
@@ -9,3 +9,9 @@ test.each`
 `('It should return the expected values', ({age, creatine, isWoman, expected}) => {
     expect(getMDRD(age, creatine, isWoman)).toEqual(expected)
 })
+
+test('It should throw error if invalid age', () => {
+    expect(() => {
+        expect(getMDRD(1000, 123, false))
+    }).toThrow('Invalid age for MDRD')
+})
--- src/mdrd.ts
+++ src/mdrd.ts
@@ -1,5 +1,11 @@
 
+const isValidAge = (age: unknown): boolean => 
+    typeof age === 'number' && age > 0 && age < 150
+
 function getMDRD(age: number, creatine: number, isWoman: boolean) {
+    if (!isValidAge(age)) {
+        throw new Error('Invalid age for MDRD')
+    }
     
     let clearance = 186.3 * (creatine / 88.4) ** (-1.154) * age ** (-0.203)
     if (isWoman) {
